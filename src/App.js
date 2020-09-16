8effe2063b2e4a30b58d49f42c5e5aa85efa5fe6
Roshan Ruzaik <mrmroshan@yahoo.com>
2020-09-16 20:25:47 +0300

    Added basic state managment and rendering the list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos,setTodos] = useState(['First todo','Second todo']);
   return (
     <div className="App">
       <h1>My React todo app</h1>
+      <input />
+      <button>Add to do</button>
+      <ul>
+        {
+          todos.map(todo => (
+            <li>{todo}</li>
+          ))
+        }
+      </ul>
     </div>
   );
 }
